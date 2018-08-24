1e84a91d2f144c1dd71e1ea10a04283b4eca15d3
David <renayanti90@gmail.com>
2018-08-25 00:02:17 +0700

    pdate

--- src/main/java/myproject/xwpf/app/Process.java
+++ src/main/java/myproject/xwpf/app/Process.java
@@ -7,12 +7,12 @@ package myproject.xwpf.app;
 
 
 import java.awt.EventQueue;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.math.BigInteger;
-import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
@@ -20,9 +20,13 @@ import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import org.apache.log4j.BasicConfigurator;
+import org.docx4j.Docx4jProperties;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.openpackaging.parts.relationships.Namespaces;
+import org.docx4j.utils.XSLTUtils;
+import sun.security.acl.WorldGroupImpl;
 /**
  *
  * @author x
@@ -31,6 +35,9 @@ public final class Process extends JPanel {
 
 	FileInputStream fc;
 	WordprocessingMLPackage document;
+//	private static Logger logger = Logger.getLogger(Process.class.getName());
+	
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			@Override
@@ -57,24 +64,48 @@ public final class Process extends JPanel {
 	}
 
 	Process(FileInputStream in) throws FileNotFoundException,IOException, Docx4JException{
+//		Logger logger = Logger.getLogger(XSLTUtils.class.getName());
+//		logger.setLevel(Level.OFF);
+		Docx4jProperties.getProperties().setProperty("docx4j.Log4j.Configurator.disabled", "true");
+//		Log4jConfigurator.configure();            
+//		org.docx4j.convert.out.pdf.viaXSLFO.Conversion.log.setLevel(Level.OFF);
 		BasicConfigurator.configure();
 		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.createPackage();
 		fc =in;
 		document  = WordprocessingMLPackage.load(in);
 		MainDocumentPart mainpart = document.getMainDocumentPart();
-		List <Object> objects = mainpart.getContent();
-		for (Object object  : objects ){
-			String string = object.toString();
-			System.out.println(string);
-		}
-		
-		
-		
+//		List <Object> objects = mainpart.getContent();
+//		for (Object object  : objects ){
+//			String string = object.toString();
+//			System.out.println(string);
+//		}
+//		
+		WordprocessingMLPackage newdoc = copy(document);
+		writeDocxToStream(newdoc,"/home/x/Documents/temp/zz.docx");
 		
 	}
 	
+	protected static WordprocessingMLPackage copy(WordprocessingMLPackage doc) throws Docx4JException {
+		WordprocessingMLPackage newdoc;
+		// Make a copy of it
+		Set<String> relationshipTypes = new TreeSet<String>();
+			relationshipTypes.add(Namespaces.DOCUMENT);
+			relationshipTypes.add(Namespaces.HEADER);
+			relationshipTypes.add(Namespaces.FOOTER);
+			//those are probably not affected but get visited by the 
+			//default TraversalUtil.
+			relationshipTypes.add(Namespaces.ENDNOTES);
+			relationshipTypes.add(Namespaces.FOOTNOTES);
+			relationshipTypes.add(Namespaces.COMMENTS);
+			newdoc = (WordprocessingMLPackage) clone.process(doc, relationshipTypes);
+			return newdoc;
+	}
 	
 	
+	private void writeDocxToStream(WordprocessingMLPackage template, String target) throws IOException, Docx4JException {
+		File f = new File(target);
+		template.save(f);
+	}
 	
 
 
